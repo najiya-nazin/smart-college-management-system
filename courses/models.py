54b2ec9b086b55d6920fe2e9ecc76c89e0033ae9
najiya <jiyaaapk@gmail.com>
2026-07-07 02:44:10 +0530

    migrations

--- courses/models.py
+++ courses/models.py
@@ -3,12 +3,26 @@ from departments.models import Department
 
 
 class Course(models.Model):
+
     name = models.CharField(max_length=100)
-    code = models.CharField(max_length=20)
-    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+    code = models.CharField(
+        max_length=20,
+        unique=True
+    )
+
+    department = models.ForeignKey(
+        Department,
+        on_delete=models.CASCADE
+    )
+
     duration = models.CharField(max_length=50)
+
     description = models.TextField()
 
+    class Meta:
+        db_table = "courses"
+        ordering = ["name"]
 
     def __str__(self):
         return self.name
